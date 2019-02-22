790e79492cd1010a527f1b3b364334785be57b84
ivomastre <40743787+ivomastre@users.noreply.github.com>
2019-02-21 22:16:57 -0300

    Teste

--- desktop/src/com/doki/game/desktop/DesktopLauncher.java
+++ desktop/src/com/doki/game/desktop/DesktopLauncher.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.doki.game.MyGdxGame;
 
-public class DesktopLauncher {
+public class  DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
